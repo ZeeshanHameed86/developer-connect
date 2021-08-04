25a795f932dac76bdb8105e7055a3c39817ffe7a
Z-Programming <designworl132@gmail.com>
2021-08-04 16:13:11 +0500

    auth-route completed

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express')
+const express = require("express");
+const connectDB = require("./config/db");
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res) => {
- res.send('API Running')
-})
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+app.use("/api/users", require("./routes/users-route"));
+app.use("/api/auth", require("./routes/auth-route"));
+app.use("/api/profile", require("./routes/profile-route"));
+app.use("/api/posts", require("./routes/posts-route"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
- console.log(`Server started on port ${PORT}`);
-})
+  console.log(`Server started on port ${PORT}`);
+});
